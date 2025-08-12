483483a9d168eee1816771106ba84bff462c44ca
chavandhananjayrajendra <chavandhananjayrajendra@gmail.com>
2025-08-13 00:52:50 +0530

    dropdown  scenarios Commit 13-08-25

--- src/main/java/org/example/selenium/examples/session2/BootStrapDropdown.java
+++ src/main/java/org/example/selenium/examples/session2/BootStrapDropdown.java
@@ -7,6 +7,7 @@ public class BootStrapDropdown {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\chava\\Downloads\\chromedriver-win64 (1)\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.get("");
 
     }
 
