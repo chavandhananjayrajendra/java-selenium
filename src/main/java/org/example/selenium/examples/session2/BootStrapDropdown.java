9dbe270dc78589c0e62222a9e438d3c55a6668a2
chavandhananjayrajendra <chavandhananjayrajendra@gmail.com>
2025-08-13 00:52:17 +0530

    dropdown  scenarios Commit 12-08-25

---
+++ src/main/java/org/example/selenium/examples/session2/BootStrapDropdown.java
@@ -0,0 +1,14 @@
+package org.example.selenium.examples.session2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BootStrapDropdown {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\chava\\Downloads\\chromedriver-win64 (1)\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+    }
+
+
+}
---
+++ src/main/java/org/example/selenium/examples/session2/SelectDropDown.java
@@ -0,0 +1,67 @@
+package org.example.selenium.examples.session2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
+
+
+/*
+ select dropdowns---
+
+
+ select class
+
+ */
+
+public class SelectDropDown {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\chava\\Downloads\\chromedriver-win64 (1)\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://testautomationpractice.blogspot.com/");
+        driver.manage().window().maximize();
+        WebElement contryEle =  driver.findElement(By.xpath("//select[@id='country']"));
+
+        //special type of class used when u see Select tagname is there on the page
+        Select country = new Select(contryEle);
+
+//select tag necessary in html to use these methods
+        //select option from dropdown
+
+        //selectByVisibleText()
+        //selectByValue()
+        //selectByIndex()
+
+        country.selectByVisibleText("France");  //select france from dropdown//innertext
+        country.selectByValue("japan");  //value  attribute start from lowercase
+
+       country.selectByIndex(2); //count manually
+        //
+        //value of a particular option element
+        // visible Text embedded in the tag
+
+        // their names may or may not be same based on the use-case and need
+        //both are different
+
+        //by using one of them u can select dropdown
+        List<WebElement> options = country.getOptions();
+        System.out.println("Total no of options available in drop down :  "+ options.size());
+
+//        for(int i = 0; i<options.size();i++){
+//            System.out.println(options.get(i).getText());
+//
+//
+//            //getoptions()----returns all options from the dropdown as a WebElement
+//        }
+//
+        for(WebElement opn: options){
+            System.out.println(opn.getText());
+        }
+
+    }
+}
